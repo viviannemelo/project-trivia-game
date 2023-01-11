f10ceec7ea5de8a3f62dbed093475e0e1bfbe230
Laísa Rio <laisa.rioverde@gmail.com>
2023-01-11 17:00:10 -0300

    store e reducer incial

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,2 +1,9 @@
 import { legacy_createStore as createStore } from 'redux';
-import { composeWithDevTools } from '@redux-devtools/extension/lib/types/logOnly';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import rootReducer from './reducers/rootReducer';
+
+const store = createStore(rootReducer, composeWithDevTools());
+if (window.Cypress) {
+  window.store = store;
+}
+export default store;
