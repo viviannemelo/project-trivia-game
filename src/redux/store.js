ebf04188a336ca4d37aafaa3001ca2dcb606aed1
Laísa Rio <laisa.rioverde@gmail.com>
2023-01-11 16:11:33 -0300

    iniciando o projeto

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,2 +1,2 @@
-// import { legacy_createStore as createStore } from 'redux';
-// import { composeWithDevTools } from '@redux-devtools/extension/lib/types/logOnly';
+import { legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension/lib/types/logOnly';
