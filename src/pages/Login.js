2e2811d7bef3e869d586174f3ccf0b4ff3387b63
carraes <carraeshb@gmail.com>
2023-01-11 19:19:55 -0300

    req 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Header from '../components/Header';
+import fetchToken from '../service/triviaServices';
 
 class Login extends Component {
   state = {
@@ -19,37 +22,60 @@ class Login extends Component {
     return verifyEmail && verifyName;
   };
 
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const { history } = this.props;
+    const { token } = await fetchToken();
+    localStorage.setItem('token', token);
+    history.push('/game');
+  };
+
   render() {
     const { name, email } = this.state;
 
     return (
-      <form>
-        <input
-          type="text"
-          placeholder="Username"
-          data-testid="input-player-name"
-          name="name"
-          value={ name }
-          onChange={ this.handleChange }
-        />
-        <input
-          type="text"
-          placeholder="E-mail"
-          name="email"
-          value={ email }
-          data-testid="input-gravatar-email"
-          onChange={ this.handleChange }
-        />
-        <button
-          type="button"
-          data-testid="btn-play"
-          disabled={ !this.validateButon() }
-        >
-          Play
-        </button>
-      </form>
+      <section>
+        <Header />
+        <div>
+          <form onSubmit={ this.handleSubmit }>
+            <input
+              type="text"
+              placeholder="Username"
+              data-testid="input-player-name"
+              name="name"
+              value={ name }
+              onChange={ this.handleChange }
+            />
+            <input
+              type="text"
+              placeholder="E-mail"
+              name="email"
+              value={ email }
+              data-testid="input-gravatar-email"
+              onChange={ this.handleChange }
+            />
+            <button
+              type="submit"
+              data-testid="btn-play"
+              disabled={ !this.validateButon() }
+            >
+              Play
+            </button>
+          </form>
+        </div>
+      </section>
     );
   }
 }
+
+Login.defaultProps = {
+  history: {},
+};
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }),
+};
 
 export default Login;
