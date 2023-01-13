e69ddd0234436df96b414249326047555932d78e
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-01-12 22:18:01 -0300

    req 8

---
+++ src/components/Timer.js
@@ -0,0 +1,52 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+const oneSecond = 1000;
+
+export class Timer extends Component {
+  interval;
+
+  state = {
+    counter: 30,
+  };
+
+  componentDidMount() {
+    this.setCounter();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { onTimeOut, isNextQuestion, notNextAnymore } = this.props;
+    const { counter } = this.state;
+    if (counter !== prevState.counter && counter === 0) {
+      clearInterval(this.interval);
+      onTimeOut();
+    }
+    if (isNextQuestion !== prevProps.isNextQuestion && isNextQuestion) {
+      this.setCounter();
+      notNextAnymore();
+    }
+  }
+
+  setCounter() {
+    clearInterval(this.interval);
+    this.setState({ counter: 30 });
+    this.interval = setInterval(this.countDown, oneSecond);
+  }
+
+  countDown = () => {
+    this.setState((state) => ({ counter: state.counter - 1 }));
+  };
+
+  render() {
+    const { counter } = this.state;
+    return <div>{counter}</div>;
+  }
+}
+
+Timer.propTypes = {
+  onTimeOut: PropTypes.func.isRequired,
+  isNextQuestion: PropTypes.bool.isRequired,
+  notNextAnymore: PropTypes.func.isRequired,
+};
+
+export default Timer;
